a93743b87acc1a691f41e8da6e3fd2895efdaefc
Zvyagintsev-kir <zvyagintsev.kirill@gmail.com>
2016-07-03 18:19:55 +0300

    test_comment

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from database import Database
 
 Database.initialize()
 
+# some new comment
 post = Post(blog_id="123",
             title="Another greate post",
             content="This is some sample content",
