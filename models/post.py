0f880641f1e122478fbb22351a6b3045161b1610
Zvyagintsev-kir <zvyagintsev.kirill@gmail.com>
2016-07-03 16:46:06 +0300

    add Database class wit a connection to mongo

--- models/post.py
+++ models/post.py
@@ -1,3 +1,6 @@
+from database import Database
+
+
 class Post(object):
 
     def __init__(self, blog_id, title, content, author, date, id):
@@ -9,7 +12,7 @@ class Post(object):
         self.id = id
 
     def save_to_mongo(self):
-        pass
+        Database.insert(collection='posts', data=self.json())
 
     def json(self):
         return {
@@ -20,3 +23,12 @@ class Post(object):
             'title': self.title,
             'created_date': self.create_date
         }
+
+    @staticmethod
+    def from_mongo(id):
+        return Database.find_one(collection='posts', query={'id': id})
+
+    @staticmethod
+    def from_blog(id):
+        return [post for post in Database.find(collection='posts', query={'blog_id': id})]
+
