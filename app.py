494523761b36aa1738f3b58adca92ced9d970aec
Kyrylo Zvyahintsev <kzvyahin@redhat.com>
2016-07-09 16:08:54 +0300

    add blog model

--- app.py
+++ app.py
@@ -8,9 +8,16 @@ post = Post(blog_id="123",
             title="Another greate post",
             content="This is some sample content",
             author="kzv")
-
-# another comment
 post.save_to_mongo()
+
+blog = Blog()
+
+blog.new_post()
+blog.save_to_mongo()
+
+Blog.from_mongo()
+
+blog.get_posts()
 
 
 
