b1fc3aaea29d564e65b960cb03153ea152d991f9
Kyrylo Zvyahintsev <kzvyahin@redhat.com>
2016-07-10 15:11:14 +0300

    add menu

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-from models.post import Post
 from database import Database
-from models.blog import Blog
+from menu import Menu
 
 Database.initialize()
 
-blog = Blog(author="Kyr",
-            title="Sample title",
-            description="Sample description")
-
-blog.new_post()
-blog.save_to_mongo()
-
-from_database = Blog.get_from_mongo(blog.id)
-
-print blog.get_posts()
+menu = Menu()
+menu.run_menu()
 
 
 
